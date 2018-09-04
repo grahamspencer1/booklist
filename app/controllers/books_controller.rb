7fd56747083898e348a541ce31ea03eb3f79e7be
Graham Spencer <grahamspence@gmail.com>
2018-09-04 15:18:08 -0400

    added csv response

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,17 @@
 class BooksController < ApplicationController
 
-def index
-  respond_to :html, :text
-end
+  def index
+
+    respond_to do |format|
+
+      format.csv do
+        render plain: Book.generate_csv(Book.all)
+      end
+
+      format.html
+      format.text
+
+    end
+  end
 
 end
